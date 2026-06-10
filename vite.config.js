0320a95e0fc594919dc0ae275ad2719b48dcac06
SweetAlpac4 <nabiltsada@gmail.com>
2026-06-10 15:45:56 +0700

    init: eldersafe mobile web app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'ElderSafe',
+        short_name: 'ElderSafe',
+        description: 'Pemantauan kesehatan lansia',
+        theme_color: '#3a5a40',
+        background_color: '#ede9e0',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
